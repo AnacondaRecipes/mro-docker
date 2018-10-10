ac3f1947c56bfcbffe32fe8a93e3232f35792bed
Ray Donnelly <mingw.android@gmail.com>
2018-10-11 00:45:30 +0100

    Find cran R exe for hardening-check too

--- cran-3.5.1/anaconda-tests.sh
+++ cran-3.5.1/anaconda-tests.sh
@@ -3,6 +3,7 @@
 [[ -d /opt/conda ]] && . /opt/conda/bin/activate
 [[ -f /opt/conda/lib/R/bin/exec/R ]] && REXE=/opt/conda/lib/R/bin/exec/R
 [[ -f /opt/microsoft/ropen/3.5.1/lib64/R/bin/exec/R ]] && REXE=/opt/microsoft/ropen/3.5.1/lib64/R/bin/exec/R
+[[ -f /usr/lib/R/bin/exec/R ]] && REXE=/usr/lib/R/bin/exec/R
 
 echo "#############"
 echo "# Hardening #"
@@ -10,6 +11,7 @@ echo "#############"
 echo ""
 ./hardening-check ${REXE}
 
+echo ""
 echo "####################"
 echo "# R-benchmark-25.R #"
 echo "####################"
